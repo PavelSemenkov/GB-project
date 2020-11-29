bc5f8bcbb73d21d68029c337490918a598a6240b
Pavel <pavelsemenkov21@gmail.co>
2020-11-29 15:49:36 +0100

    vue add

--- script/vuetest.js
+++ script/vuetest.js
@@ -1,2 +1,32 @@
 'use strict';
+const app = new Vue({
+    el: '#root',
+    data: {
+        name: ''
+    },
+    computed: {
+        title(){
+            return 'Hello!' + this.name;
+        }
+    },
+    methods: {
+        clear(event){
+            this.name = '';
+        },
+        getData(){
+            fetch(
+                "https://raw.githubusercontent.com/PavelSemenkov/GB-project/JS2Lesson03/response/products.json",
+                {
+                    method: 'GET',
+                    headers: {},
+                    // body: ''
+                }
+            ).then(res => res.json()).then(res => {
+                    this.itemms = res;
+            });
+        }
+    }
+});
+
+console.log(app);
 
